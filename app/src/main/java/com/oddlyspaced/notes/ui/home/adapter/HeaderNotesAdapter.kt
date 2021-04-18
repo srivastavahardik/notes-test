593fc54145a73c2199f725e3633cfb0164f83aed
srivastavahardik <srivastavahardik@gmail.com>
2021-04-18 19:20:02 +0530

    HeaderNotesAdapter: Use data binding to inflate

--- app/src/main/java/com/oddlyspaced/notes/ui/home/adapter/HeaderNotesAdapter.kt
+++ app/src/main/java/com/oddlyspaced/notes/ui/home/adapter/HeaderNotesAdapter.kt
@@ -1,17 +1,16 @@
 package com.oddlyspaced.notes.ui.home.adapter
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import com.oddlyspaced.notes.R
+import com.oddlyspaced.notes.databinding.ItemNoteBinding
 
 class HeaderNotesAdapter : RecyclerView.Adapter<HeaderNotesAdapter.NoteViewHolder>() {
 
-    inner class NoteViewHolder(binding: View) : RecyclerView.ViewHolder(binding)
+    inner class NoteViewHolder(binding: ItemNoteBinding) : RecyclerView.ViewHolder(binding.root)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteViewHolder {
-        return NoteViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_note, parent, false))
+        return NoteViewHolder(ItemNoteBinding.inflate(LayoutInflater.from(parent.context)))
     }
 
     override fun onBindViewHolder(holder: NoteViewHolder, position: Int) {
