b40382ce180f95a22bf0c314787dcf3c4b27cb24
srivastavahardik <srivastavahardik@gmail.com>
2021-04-18 22:14:17 +0530

    HomeFragment: Use HomeViewModel

--- app/src/main/java/com/oddlyspaced/notes/ui/home/fragment/HomeFragment.kt
+++ app/src/main/java/com/oddlyspaced/notes/ui/home/fragment/HomeFragment.kt
@@ -1,34 +1,47 @@
 package com.oddlyspaced.notes.ui.home.fragment
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.oddlyspaced.notes.R
 import com.oddlyspaced.notes.databinding.FragmentHomeBinding
-import com.oddlyspaced.notes.ui.home.adapter.HeaderNotesAdapter
+import com.oddlyspaced.notes.ui.home.adapter.NotesAdapter
+import com.oddlyspaced.notes.ui.home.viewmodel.HomeViewModel
 
 class HomeFragment : Fragment() {
 
     private lateinit var binding: FragmentHomeBinding
+    private lateinit var viewmodel: HomeViewModel
+
+    private lateinit var adapter: NotesAdapter
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        viewmodel = ViewModelProvider(this).get(HomeViewModel::class.java)
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_home, container, false)
+
+        setupRecyclerView()
+        setupObservers()
+
         return binding.root
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        displaySampleHeaders()
+    private fun setupRecyclerView() {
+        binding.rvHomeHeader.layoutManager = StaggeredGridLayoutManager(2, LinearLayoutManager.VERTICAL)
+        adapter = NotesAdapter()
+        binding.rvHomeHeader.adapter = adapter
     }
 
-    private fun displaySampleHeaders() {
-//        binding.rvHomeHeader.layoutManager = LinearLayoutManager(activity)
-        binding.rvHomeHeader.layoutManager = StaggeredGridLayoutManager(2, LinearLayoutManager.VERTICAL)
-        binding.rvHomeHeader.adapter = HeaderNotesAdapter()
+    private fun setupObservers() {
+        viewmodel.notes.observe(viewLifecycleOwner, {
+            adapter.submitList(it)
+        })
     }
 
 }
