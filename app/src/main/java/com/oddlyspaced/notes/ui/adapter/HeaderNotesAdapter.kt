226b55acb5b9862bbd55ba611d50b60a28897cb7
srivastavahardik <srivastavahardik@gmail.com>
2021-04-18 18:50:10 +0530

    HeaderNotesAdapter: Setup sample base class

---
+++ app/src/main/java/com/oddlyspaced/notes/ui/adapter/HeaderNotesAdapter.kt
@@ -0,0 +1,24 @@
+package com.oddlyspaced.notes.ui.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.oddlyspaced.notes.databinding.ItemNoteBinding
+
+class HeaderNotesAdapter : RecyclerView.Adapter<HeaderNotesAdapter.NoteViewHolder>() {
+
+    inner class NoteViewHolder(binding: ItemNoteBinding) : RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteViewHolder {
+        return NoteViewHolder(ItemNoteBinding.inflate(LayoutInflater.from(parent.context)))
+    }
+
+    override fun onBindViewHolder(holder: NoteViewHolder, position: Int) {
+
+    }
+
+    override fun getItemCount(): Int {
+        return 10
+    }
+
+}
