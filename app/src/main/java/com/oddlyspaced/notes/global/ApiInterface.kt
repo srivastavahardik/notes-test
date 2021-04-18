1a4372d473b0ddb1af3de80466678a5fb1878d77
srivastavahardik <srivastavahardik@gmail.com>
2021-04-18 22:20:58 +0530

    Global: Setup constant file

---
+++ app/src/main/java/com/oddlyspaced/notes/global/ApiInterface.kt
@@ -0,0 +1,11 @@
+package com.oddlyspaced.notes.global
+
+import com.oddlyspaced.notes.modal.Note
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface ApiInterface {
+    @GET("note/get")
+    fun fetchNotes(@Query("folderId") folderId: Int = 1): Call<List<Note>>
+}
---
+++ app/src/main/java/com/oddlyspaced/notes/global/Global.kt
@@ -0,0 +1,7 @@
+package com.oddlyspaced.notes.global
+
+class Global {
+    companion object {
+        const val API_URL = "http://localhost:8080/"
+    }
+}
