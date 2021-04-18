89c6cd24bc3389bbad2f1c48b42e49b06b17a907
srivastavahardik <srivastavahardik@gmail.com>
2021-04-18 19:21:01 +0530

    HomeFragment: Show recycler view sample items

--- app/src/main/java/com/oddlyspaced/notes/ui/home/fragment/HomeFragment.kt
+++ app/src/main/java/com/oddlyspaced/notes/ui/home/fragment/HomeFragment.kt
@@ -1,13 +1,16 @@
-package com.oddlyspaced.notes.ui.fragment
+package com.oddlyspaced.notes.ui.home.fragment
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.oddlyspaced.notes.R
 import com.oddlyspaced.notes.databinding.FragmentHomeBinding
+import com.oddlyspaced.notes.ui.home.adapter.HeaderNotesAdapter
 
 class HomeFragment : Fragment() {
 
@@ -17,4 +20,14 @@ class HomeFragment : Fragment() {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_home, container, false)
         return binding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        displaySampleHeaders()
+    }
+
+    private fun displaySampleHeaders() {
+        binding.rvHomeHeader.layoutManager = LinearLayoutManager(activity, LinearLayoutManager.HORIZONTAL, false)
+        binding.rvHomeHeader.adapter = HeaderNotesAdapter()
+    }
+
 }
