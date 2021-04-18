6d2720495e83a8a6721c3a7a50985190904a303b
srivastavahardik <srivastavahardik@gmail.com>
2021-04-18 22:47:18 +0530

    Global: Set proper ip

--- app/src/main/java/com/oddlyspaced/notes/global/Global.kt
+++ app/src/main/java/com/oddlyspaced/notes/global/Global.kt
@@ -2,6 +2,6 @@ package com.oddlyspaced.notes.global
 
 class Global {
     companion object {
-        const val API_URL = "http://localhost:8080/"
+        const val API_URL = "http://192.168.29.185:8080/"
     }
 }
