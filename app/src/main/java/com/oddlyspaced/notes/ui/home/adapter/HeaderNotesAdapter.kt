ca680d9374e0060d80829b2e14a1125f9ea88c85
srivastavahardik <srivastavahardik@gmail.com>
2021-04-18 19:18:42 +0530

    HeaderNotesAdapter: Manually inflate view for now

--- app/src/main/java/com/oddlyspaced/notes/ui/home/adapter/HeaderNotesAdapter.kt
+++ app/src/main/java/com/oddlyspaced/notes/ui/home/adapter/HeaderNotesAdapter.kt
@@ -1,16 +1,17 @@
-package com.oddlyspaced.notes.ui.adapter
+package com.oddlyspaced.notes.ui.home.adapter
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import com.oddlyspaced.notes.databinding.ItemNoteBinding
+import com.oddlyspaced.notes.R
 
 class HeaderNotesAdapter : RecyclerView.Adapter<HeaderNotesAdapter.NoteViewHolder>() {
 
-    inner class NoteViewHolder(binding: ItemNoteBinding) : RecyclerView.ViewHolder(binding.root)
+    inner class NoteViewHolder(binding: View) : RecyclerView.ViewHolder(binding)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteViewHolder {
-        return NoteViewHolder(ItemNoteBinding.inflate(LayoutInflater.from(parent.context)))
+        return NoteViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_note, parent, false))
     }
 
     override fun onBindViewHolder(holder: NoteViewHolder, position: Int) {
