ba298790f72b3aede88c98fef27bd0f9092ca751
srivastavahardik <srivastavahardik@gmail.com>
2021-04-18 18:49:53 +0530

    HomeFragment: Bind class to layout

--- app/src/main/java/com/oddlyspaced/notes/ui/fragment/HomeFragment.kt
+++ app/src/main/java/com/oddlyspaced/notes/ui/fragment/HomeFragment.kt
@@ -1,7 +1,20 @@
 package com.oddlyspaced.notes.ui.fragment
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.oddlyspaced.notes.R
+import com.oddlyspaced.notes.databinding.FragmentHomeBinding
 
-class HomeFragment: Fragment(R.layout.fragment_home) {
+class HomeFragment : Fragment() {
+
+    private lateinit var binding: FragmentHomeBinding
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_home, container, false)
+        return binding.root
+    }
 }
