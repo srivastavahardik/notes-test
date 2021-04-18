233bddc52db3029d4e22ab59161b0c971e7a83fd
srivastavahardik <srivastavahardik@gmail.com>
2021-04-19 00:31:11 +0530

    NoteFragment: Setup base layout

--- app/src/main/java/com/oddlyspaced/notes/ui/note/fragment/NoteFragment.kt
+++ app/src/main/java/com/oddlyspaced/notes/ui/note/fragment/NoteFragment.kt
@@ -1,6 +1,22 @@
 package com.oddlyspaced.notes.ui.note.fragment
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import com.oddlyspaced.notes.R
+import com.oddlyspaced.notes.databinding.FragmentNoteBinding
 
 class NoteFragment: Fragment() {
+
+    private lateinit var binding: FragmentNoteBinding
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_note, container, false)
+        return binding.root
+    }
+
+
 }
