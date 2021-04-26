bc7a31020fba329e7521e488216b37c865680f5b
srivastavahardik <srivastavahardik@gmail.com>
2021-04-26 17:22:16 +0530

    NoteFragment: Fetch and observe

--- app/src/main/java/com/oddlyspaced/notes/ui/note/fragment/NoteFragment.kt
+++ app/src/main/java/com/oddlyspaced/notes/ui/note/fragment/NoteFragment.kt
@@ -8,14 +8,37 @@ import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.oddlyspaced.notes.R
 import com.oddlyspaced.notes.databinding.FragmentNoteBinding
+import com.oddlyspaced.notes.ui.note.viewmodel.NoteViewModel
+import com.oddlyspaced.notes.ui.note.viewmodel.NoteViewModelFactory
 
 class NoteFragment: Fragment() {
 
     private lateinit var binding: FragmentNoteBinding
+    private lateinit var viewmodel: NoteViewModel
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        viewmodel = NoteViewModelFactory(1).create(NoteViewModel::class.java)
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_note, container, false)
+
+        setupObservers()
+
         return binding.root
+    }
+
+    private fun setupObservers() {
+        viewmodel.note.observe(viewLifecycleOwner, { completeNote ->
+            completeNote.note.let { note ->
+                binding.txTitle.setText(note.title)
+                binding.txDate.text = note.date
+            }
+
+            binding.txContent.setText("")
+            completeNote.content.let { contentList ->
+                if (contentList.isNotEmpty()) {
+                    binding.txContent.setText(contentList.first().content)
+                }
+            }
+        })
     }
 
 
