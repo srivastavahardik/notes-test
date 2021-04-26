499e52aa89ddfcb731898a4067e4309cff60ea4f
srivastavahardik <srivastavahardik@gmail.com>
2021-04-26 18:01:06 +0530

    NoteViewModel: Add live data for edit state

--- app/src/main/java/com/oddlyspaced/notes/ui/note/viewmodel/NoteViewModel.kt
+++ app/src/main/java/com/oddlyspaced/notes/ui/note/viewmodel/NoteViewModel.kt
@@ -1,5 +1,8 @@
 package com.oddlyspaced.notes.ui.note.viewmodel
 
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.oddlyspaced.notes.repository.NotesRepository
 
@@ -7,5 +10,19 @@ class NoteViewModel(noteID: Int): ViewModel() {
 
     private val repository = NotesRepository
     val note = repository.fetchNoteInfo(noteID)
+
+    private val _isEditing = MutableLiveData<Boolean>()
+    val isEditing: LiveData<Boolean>
+        get() = _isEditing
+
+    init {
+        _isEditing.postValue(false)
+    }
+
+    fun toggleEditing() {
+        val toggleValue = _isEditing.value?: true
+        Log.e("togg", toggleValue.toString())
+        _isEditing.postValue(!toggleValue)
+    }
 
 }
