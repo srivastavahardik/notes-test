77643710aba14ea8a600c8395621625453cc8a36
srivastavahardik <srivastavahardik@gmail.com>
2021-04-27 00:59:49 +0530

    ApiInterface: Cleanup Interface for now

--- app/src/main/java/com/oddlyspaced/notes/api/ApiInterface.kt
+++ app/src/main/java/com/oddlyspaced/notes/api/ApiInterface.kt
@@ -2,7 +2,9 @@ package com.oddlyspaced.notes.api
 
 import com.oddlyspaced.notes.modal.*
 import retrofit2.Call
+import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.POST
 import retrofit2.http.Query
 
 interface ApiInterface {
@@ -11,15 +13,8 @@ interface ApiInterface {
     @GET("/folder/all")
     fun fetchFoldersAll(): Call<List<Folder>>
 
-    @GET("/folder/exists")
-    fun checkFolderExists(): Call<MessageResponse>
-
-    @GET("note/get")
-    fun fetchNotes(@Query("folderId") folderId: Int = 1): Call<List<Note>>
-
-    @GET("note/fetch")
-    fun fetchNote(@Query("noteId") noteId: Int): Call<CompleteNote>
-
-    @GET("/item/get")
-    fun fetchItems(@Query("noteId") noteId: Int): Call<List<Item>>
+    @GET("/folder/notes")
+    fun fetchNotesInFolder(
+        @Query("folderId") folderId: Int = 1
+    ): Call<List<Note>>
 }
