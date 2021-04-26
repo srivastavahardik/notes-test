47bda960636fc0fcac0c8cc7f8999e8f6e0ec385
srivastavahardik <srivastavahardik@gmail.com>
2021-04-26 16:21:59 +0530

    ApiInterface: Add definitive GET Request end points

--- app/src/main/java/com/oddlyspaced/notes/api/ApiInterface.kt
+++ app/src/main/java/com/oddlyspaced/notes/api/ApiInterface.kt
@@ -1,11 +1,25 @@
 package com.oddlyspaced.notes.api
 
-import com.oddlyspaced.notes.modal.Note
+import com.oddlyspaced.notes.modal.*
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface ApiInterface {
+
+    // Folder Related
+    @GET("/folder/all")
+    fun fetchFoldersAll(): Call<List<Folder>>
+
+    @GET("/folder/exists")
+    fun checkFolderExists(): Call<MessageResponse>
+
     @GET("note/get")
     fun fetchNotes(@Query("folderId") folderId: Int = 1): Call<List<Note>>
+
+    @GET("note/fetch")
+    fun fetchNote(@Query("noteId") noteId: Int): Call<CompleteNote>
+
+    @GET("/item/get")
+    fun fetchItems(@Query("noteId") noteId: Int): Call<List<Item>>
 }
